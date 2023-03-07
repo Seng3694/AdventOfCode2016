8064298c8ca3c5910dc494699da5f3d68d0158a0
Seng <engelhardt_stefan@gmx.de>
2023-03-07 19:13:43 +0100

    added day 1 part 2

--- day01/main.c
+++ day01/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <math.h>
 
@@ -25,6 +26,13 @@ typedef struct {
     int x;
     int y;
 } position;
+
+static const position MOVE_POSITIONS[] = {
+    [FACE_DIR_NORTH] = { 0, -1},
+    [FACE_DIR_EAST]  = { 1,  0},
+    [FACE_DIR_SOUTH] = { 0,  1},
+    [FACE_DIR_WEST]  = {-1,  0},
+};
 
 static bool read_file_to_string(const char *path, char **output, size_t *length) {
     FILE *file = fopen(path, "r");
@@ -80,20 +88,8 @@ static inline int distance(const position pos) {
 }
 
 static inline void move(position *pos, const face_direction direction, const int amount) {
-    switch(direction) {
-    case FACE_DIR_NORTH:
-        pos->y -= amount;
-        break;
-    case FACE_DIR_EAST:
-        pos->x += amount;
-        break;
-    case FACE_DIR_SOUTH:
-        pos->y += amount;
-        break;
-    case FACE_DIR_WEST:
-        pos->x -= amount;
-        break;
-    }
+    pos->x += MOVE_POSITIONS[direction].x * amount;
+    pos->y += MOVE_POSITIONS[direction].y * amount;
 }
 
 static int solve_part1(const instruction *instructions, const size_t count) {
@@ -105,6 +101,61 @@ static int solve_part1(const instruction *instructions, const size_t count) {
         dir = wrap(dir + change, 0, 3);
         move(&pos, dir, current->length);
     }
+    return distance(pos);
+}
+
+static inline uint32_t hash_position(const position pos) {
+    uint32_t hash = 2166136261u;
+    hash ^= pos.x;
+    hash *= 16777619;
+    hash ^= pos.y;
+    hash *= 16777619;
+    return hash;
+}
+
+static int solve_part2(const instruction *instructions, const size_t count) {
+    position pos = {0, 0};
+    face_direction dir = FACE_DIR_NORTH;
+    //sum up lengths to know how many points there can be
+    size_t positionsCapacity = 0;
+    for(size_t i = 0; i < count; ++i) {
+        positionsCapacity += instructions[i].length;
+    }
+    //add 50% extra so probing won't take too long to find empty spots
+    positionsCapacity *= 1.5;
+    //sentinel value
+    const uint32_t unset = 0xdeadb0b1; 
+    uint32_t *positions = malloc(sizeof(uint32_t) * positionsCapacity);
+    for(size_t i = 0; i < positionsCapacity; ++i) {
+        positions[i] = unset;
+    }
+
+    uint32_t hash = hash_position(pos);
+    size_t index = hash % positionsCapacity;
+    positions[index] = hash;
+
+    for(size_t i = 0; i < count; ++i) {
+        const instruction *current = &instructions[i];
+        const int change = current->direction == TURN_DIR_RIGHT ? 1 : -1; 
+        dir = wrap(dir + change, 0, 3);
+
+        for(size_t j = 0; j < current->length; ++j) {
+            move(&pos, dir, 1);
+            hash = hash_position(pos);
+            index = hash % positionsCapacity;
+            //probe for free spot
+            while(positions[index] != unset && positions[index] != hash) {
+                index = (index + 1) % positionsCapacity;
+            }
+            if (positions[index] == hash) {
+                goto done;
+            }
+            positions[index] = hash;
+        }
+    }
+    
+done:
+    free(positions);
     return distance(pos);
 }
 
@@ -120,8 +171,10 @@ int main(void) {
     parse(input, &instructions, &instructionCount);
 
     const int part1 = solve_part1(instructions, instructionCount);
+    const int part2 = solve_part2(instructions, instructionCount);
 
     printf("%d\n", part1);
+    printf("%d\n", part2);
 
     free(instructions);
     free(input);
