f08874e0a13e0223b187f26302e1918448408105
Seng <engelhardt_stefan@gmx.de>
2023-03-08 20:06:09 +0100

    refactored and solved day 2 part 1

--- day02/main.c
+++ day02/main.c
@@ -1,56 +1,86 @@
 #include <stdio.h>
+#include <string.h>
 #include <aux.h>
 
-typedef enum {
-  MOVE_DIRECTION_UP,
-  MOVE_DIRECTION_RIGHT,
-  MOVE_DIRECTION_DOWN,
-  MOVE_DIRECTION_LEFT,
-} move_direction;
+typedef struct {
+  int8_t x;
+  int8_t y;
+} position;
 
-#define ARRAY_T move_direction
-#define ARRAY_T_NAME Direction
+#define ARRAY_T char
+#define ARRAY_T_NAME Char
 #include <aux_array.h>
 
-#define ARRAY_T AuxArrayDirection
-#define ARRAY_T_NAME Instr
+#define ARRAY_T AuxArrayChar
+#define ARRAY_T_NAME String
 #include <aux_array.h>
 
 void parse_line(char *line, size_t length, void *userData) {
-  AuxArrayDirection directions;
-  AuxArrayDirectionCreate(&directions, 256);
+  AuxArrayString *arr = (AuxArrayString *)userData;
+  AuxArrayChar str;
+  AuxArrayCharCreate(&str, length + 1);
+  memcpy(str.items, line, length + 1);
+  str.length = length;
+  AuxArrayStringPush(arr, str);
+}
 
-  AuxRemoveTrailingWhitespace(line, &length);
-  for (size_t i = 0; i < length; ++i) {
-    switch (*line) {
-    case 'L':
-      AuxArrayDirectionPush(&directions, MOVE_DIRECTION_LEFT);
-      break;
-    case 'U':
-      AuxArrayDirectionPush(&directions, MOVE_DIRECTION_UP);
-      break;
-    case 'R':
-      AuxArrayDirectionPush(&directions, MOVE_DIRECTION_RIGHT);
-      break;
-    case 'D':
-      AuxArrayDirectionPush(&directions, MOVE_DIRECTION_DOWN);
-      break;
-    default:
-      AUX_LOG("Invalid direction '%c' (0x%x)\n", *line, *line);
-      break;
+static inline int8_t clamp(const int8_t value, const int8_t min,
+                           const int8_t max) {
+  if (value < min)
+    return min;
+  else if (value > max)
+    return max;
+  else
+    return value;
+}
+
+static int solve_part1(const AuxArrayString *const instructions) {
+  AuxArrayChar code;
+  AuxArrayCharCreate(&code, 32);
+  memset(code.items, 0, code.capacity);
+
+  // 1 2 3
+  // 4 5 6
+  // 7 8 9
+  position pos = {1, 1}; // start at 5
+
+  for (size_t i = 0; i < instructions->length; ++i) {
+    const AuxArrayChar *const directions = &instructions->items[i];
+    for (size_t j = 0; j < directions->length; ++j) {
+      switch (directions->items[j]) {
+      case 'U':
+        pos.y = clamp(pos.y - 1, 0, 2);
+        break;
+      case 'R':
+        pos.x = clamp(pos.x + 1, 0, 2);
+        break;
+      case 'D':
+        pos.y = clamp(pos.y + 1, 0, 2);
+        break;
+      case 'L':
+        pos.x = clamp(pos.x - 1, 0, 2);
+        break;
+      }
     }
+    printf("pos: %d %d\n", pos.x, pos.y);
+    AuxArrayCharPush(&code, '1' + (pos.y * 3 + pos.x));
   }
-  AuxArrayInstr *arr = (AuxArrayInstr *)userData;
-  AuxArrayInstrPush(arr, directions);
+  const int solution = (int)strtol(code.items, NULL, 10);
+  AuxArrayCharDestroy(&code);
+  return solution;
 }
 
 int main(void) {
-  AuxArrayInstr instructions;
-  AuxArrayInstrCreate(&instructions, 8);
+  AuxArrayString instructions;
+  AuxArrayStringCreate(&instructions, 8);
   AuxReadFileLineByLine("day02/input.txt", parse_line, &instructions);
 
+  const int part1 = solve_part1(&instructions);
+
+  printf("%d\n", part1);
+
   for (size_t i = 0; i < instructions.length; ++i) {
-    AuxArrayDirectionDestroy(&instructions.items[i]);
+    AuxArrayCharDestroy(&instructions.items[i]);
   }
-  AuxArrayInstrDestroy(&instructions);
+  AuxArrayStringDestroy(&instructions);
 }
