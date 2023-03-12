a1cd35991a459adbd666a17eb0b697c48a1c0c09
Seng <engelhardt_stefan@gmx.de>
2023-03-12 20:13:01 +0100

    added day 5 part 2

--- day05/main.c
+++ day05/main.c
@@ -8,8 +8,12 @@ static inline bool starts_with_5_zeroes(const uint8_t *const hash) {
   return hash[0] == 0 && hash[1] == 0 && ((hash[2] & 0xf0) == 0);
 }
 
-static inline char get_sixth_hash_digit(const uint8_t *const hash) {
-  return hex_lookup[hash[2] & 0xf];
+static inline uint8_t get_sixth_hash_digit(const uint8_t *const hash) {
+  return hash[2] & 0xf;
+}
+
+static inline uint8_t get_seventh_hash_digit(const uint8_t *const hash) {
+  return (hash[3] & 0xf0) >> 4;
 }
 
 static void solve_part1(const char input[], const size_t inputLength,
@@ -26,7 +30,29 @@ static void solve_part1(const char input[], const size_t inputLength,
     sprintf(start, "%zu", i);
     AuxMD5(buffer, strlen(buffer), hashResult);
     if (starts_with_5_zeroes(hashResult)) {
-      output[solutionLength++] = get_sixth_hash_digit(hashResult);
+      output[solutionLength++] = hex_lookup[get_sixth_hash_digit(hashResult)];
+    }
+  }
+}
+
+static void solve_part2(const char input[], const size_t inputLength,
+                        char *const output) {
+  // init with sentinel value
+  memset(output, '?', 8);
+  char buffer[256] = {0};
+  memcpy(buffer, input, inputLength);
+  char *start = buffer + inputLength;
+  size_t solutionLength = 0;
+  uint8_t hashResult[16] = {0};
+  for (size_t i = 0; solutionLength < 8; ++i) {
+    sprintf(start, "%zu", i);
+    AuxMD5(buffer, strlen(buffer), hashResult);
+    if (starts_with_5_zeroes(hashResult)) {
+      const uint8_t index = get_sixth_hash_digit(hashResult);
+      if (index < 8 && output[index] == '?') {
+        output[index] = hex_lookup[get_seventh_hash_digit(hashResult)];
+        solutionLength++;
+      }
     }
   }
 }
@@ -35,7 +61,11 @@ int main(void) {
   const char input[] = "wtnhxymk";
   const size_t inputLength = sizeof(input) - 1;
   char part1[9] = {0};
+  char part2[9] = {0};
+
   solve_part1(input, inputLength, part1);
+  solve_part2(input, inputLength, part2);
 
   printf("%s\n", part1);
+  printf("%s\n", part2);
 }
