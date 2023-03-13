f19a2b219bbe57d71eba8a3183abbff075c631f7
Seng <engelhardt_stefan@gmx.de>
2023-03-13 19:12:31 +0100

    improved performance by solving both parts in the same go

--- day05/main.c
+++ day05/main.c
@@ -16,42 +16,26 @@ static inline uint8_t get_seventh_hash_digit(const uint8_t *const hash) {
   return (hash[3] & 0xf0) >> 4;
 }
 
-static void solve_part1(const char input[], const size_t inputLength,
-                        char *const output) {
-  // buffer to store the string to hash
-  char buffer[256] = {0};
-  memcpy(buffer, input, inputLength);
-  // points to where the numbers will be written to
-  char *start = buffer + inputLength;
-  size_t solutionLength = 0;
-  // buffer for the md5 hashes
-  uint8_t hashResult[16] = {0};
-  for (size_t i = 0; solutionLength < 8; ++i) {
-    sprintf(start, "%zu", i);
-    AuxMD5(buffer, strlen(buffer), hashResult);
-    if (starts_with_5_zeroes(hashResult)) {
-      output[solutionLength++] = hex_lookup[get_sixth_hash_digit(hashResult)];
-    }
-  }
-}
-
-static void solve_part2(const char input[], const size_t inputLength,
-                        char *const output) {
+static void solve_both_parts(const char input[], const size_t inputLength,
+                             char *const part1, char *const part2) {
   // init with sentinel value
-  memset(output, '?', 8);
+  memset(part2, '?', 8);
   char buffer[256] = {0};
   memcpy(buffer, input, inputLength);
   char *start = buffer + inputLength;
-  size_t solutionLength = 0;
+  size_t solutionLength1 = 0;
+  size_t solutionLength2 = 0;
   uint8_t hashResult[16] = {0};
-  for (size_t i = 0; solutionLength < 8; ++i) {
+  for (size_t i = 0; solutionLength2 < 8; ++i) {
     sprintf(start, "%zu", i);
     AuxMD5(buffer, strlen(buffer), hashResult);
     if (starts_with_5_zeroes(hashResult)) {
       const uint8_t index = get_sixth_hash_digit(hashResult);
-      if (index < 8 && output[index] == '?') {
-        output[index] = hex_lookup[get_seventh_hash_digit(hashResult)];
-        solutionLength++;
+      if (solutionLength1 < 8)
+        part1[solutionLength1++] = hex_lookup[index];
+      if (index < 8 && part2[index] == '?') {
+        part2[index] = hex_lookup[get_seventh_hash_digit(hashResult)];
+        solutionLength2++;
       }
     }
   }
@@ -63,8 +47,7 @@ int main(void) {
   char part1[9] = {0};
   char part2[9] = {0};
 
-  solve_part1(input, inputLength, part1);
-  solve_part2(input, inputLength, part2);
+  solve_both_parts(input, inputLength, part1, part2);
 
   printf("%s\n", part1);
   printf("%s\n", part2);
