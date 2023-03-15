2f6449074781c463049f463de3f90c42d3fd7776
Seng <engelhardt_stefan@gmx.de>
2023-03-15 18:28:56 +0100

    added day 7 part 2

--- day07/main.c
+++ day07/main.c
@@ -14,8 +14,17 @@ typedef struct {
   size_t bracketsCount;
 } ipv7_address;
 
+typedef struct {
+  char a;
+  char b;
+} aba_data;
+
 #define ARRAY_T ipv7_address
 #define ARRAY_T_NAME Ipv7
+#include <aux_array.h>
+
+#define ARRAY_T aba_data
+#define ARRAY_T_NAME AbaData
 #include <aux_array.h>
 
 static void parse(char *line, size_t length, void *userData) {
@@ -61,6 +70,25 @@ static bool has_abba_pattern(const char *const str, const size_t length) {
   return false;
 }
 
+static void get_all_aba_patterns(const char *const str, const size_t length,
+                                 AuxArrayAbaData *const abaDataArr) {
+  for (size_t i = 0; i + 2 < length; ++i) {
+    if (str[i] == str[i + 2] && str[i] != str[i + 1]) {
+      AuxArrayAbaDataPush(abaDataArr, (aba_data){.a = str[i], .b = str[i + 1]});
+    }
+  }
+}
+
+static bool has_specific_aba_pattern(const char *const str, const size_t length,
+                                     const char a, const char b) {
+  for (size_t i = 0; i + 2 < length; ++i) {
+    if (str[i] == a && str[i + 1] == b && str[i + 2] == a) {
+      return true;
+    }
+  }
+  return false;
+}
+
 static uint32_t solve_part1(const AuxArrayIpv7 *const addresses) {
   uint32_t count = 0;
   for (size_t i = 0; i < addresses->length; ++i) {
@@ -81,13 +109,43 @@ static uint32_t solve_part1(const AuxArrayIpv7 *const addresses) {
   return count;
 }
 
+static uint32_t solve_part2(const AuxArrayIpv7 *const addresses) {
+  uint32_t count = 0;
+  AuxArrayAbaData abaDataArr;
+  AuxArrayAbaDataCreate(&abaDataArr, 16);
+  for (size_t i = 0; i < addresses->length; ++i) {
+    ipv7_address *adr = &addresses->items[i];
+    for (size_t j = 0; j < adr->bracketsCount; ++j) {
+      get_all_aba_patterns(adr->brackets[j].start, adr->brackets[j].length,
+                           &abaDataArr);
+      for (size_t k = 0; k < abaDataArr.length; ++k) {
+        const aba_data *const aba = &abaDataArr.items[k];
+        for (size_t m = 0; m < adr->stringsCount; ++m) {
+          if (has_specific_aba_pattern(adr->strings[m].start,
+                                       adr->strings[m].length, aba->b,
+                                       aba->a)) {
+            count++;
+            goto next_address;
+          }
+        }
+      }
+    }
+  next_address:
+    AuxArrayAbaDataClear(&abaDataArr);
+  }
+  AuxArrayAbaDataDestroy(&abaDataArr);
+  return count;
+}
+
 int main(void) {
   AuxArrayIpv7 addresses;
   AuxArrayIpv7Create(&addresses, 2004);
   AuxReadFileLineByLine("day07/input.txt", parse, &addresses);
 
   const uint32_t part1 = solve_part1(&addresses);
+  const uint32_t part2 = solve_part2(&addresses);
   printf("%u\n", part1);
+  printf("%u\n", part2);
 
   AuxArrayIpv7Destroy(&addresses);
 }
