5d0c64b4f015dc93a46bbdccb982e662c02a3e65
Seng <engelhardt_stefan@gmx.de>
2023-03-29 16:04:20 +0200

    optimized part 1

--- day19/main.c
+++ day19/main.c
@@ -12,30 +12,44 @@ static inline uint32_t find_next(const uint32_t *const gifts, uint32_t current,
   return current;
 }
 
+static void print_gifts(const uint32_t *const gifts, const uint32_t count) {
+  for (uint32_t i = 0; i < count; ++i) {
+    putc(gifts[i] == 0 ? '.' : '#', stdout);
+  }
+  printf("\n");
+}
+
 static uint32_t solve_part1(const uint32_t elves) {
   uint32_t *gifts = malloc(sizeof(uint32_t) * elves);
   for (uint32_t i = 0; i < elves; ++i) {
     gifts[i] = 1;
   }
-  const uint32_t runs = (uint32_t)log2((double)elves);
+
+  const uint32_t rounds = (uint32_t)log2((double)elves);
 
   uint32_t last = 0;
-  for (uint32_t i = 0; i < runs; ++i) {
-    for (uint32_t e = 0; e < elves; ++e) {
+  uint32_t current = 0;
+  for (uint32_t r = 0; r < rounds; ++r) {
+    uint32_t step = pow(2, r);
+
+    for (uint32_t e = current; e < elves; e += step) {
       if (gifts[e] == 0) {
         continue;
       }
-      const size_t next = find_next(gifts, e, elves);
-      gifts[e] += gifts[next];
-      gifts[next] = 0;
+      current = find_next(gifts, e + step - 1, elves);
+      gifts[e] += gifts[current];
+      gifts[current] = 0;
       last = e;
     }
+    current = find_next(gifts, current, elves);
   }
 
+  free(gifts);
   return last + 1;
 }
 
 int main(void) {
+
   const uint32_t input = 3004953;
   const uint32_t part1 = solve_part1(input);
   printf("%u\n", part1);
