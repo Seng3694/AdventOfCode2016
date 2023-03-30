dec8ac2de69825bddfbfe2cad4584b106dcd414c
Seng <engelhardt_stefan@gmx.de>
2023-03-31 00:23:58 +0200

    solved day 20 part 2

--- day20/main.c
+++ day20/main.c
@@ -45,6 +45,20 @@ static uint64_t solve_part1(const AuxArrayRange *const ranges) {
   return lowest;
 }
 
+static uint64_t solve_part2(const AuxArrayRange *const ranges) {
+  uint64_t current = 0;
+  uint64_t allowed = 0;
+  for (size_t i = 0; i < ranges->length; ++i) {
+    const range *const r = &ranges->items[i];
+    if (r->from > current) {
+      allowed += r->from - current;
+      current = r->to + 1;
+    } else if (r->to > current)
+      current = r->to + 1;
+  }
+  return allowed;
+}
+
 int main(void) {
   AuxArrayRange ranges = {0};
   AuxArrayRangeCreate(&ranges, 1080);
@@ -52,8 +66,10 @@ int main(void) {
   qsort(ranges.items, ranges.length, sizeof(range), compare_range);
 
   const uint64_t part1 = solve_part1(&ranges);
+  const uint64_t part2 = solve_part2(&ranges);
 
   printf("%lu\n", part1);
+  printf("%lu\n", part2);
 
   AuxArrayRangeDestroy(&ranges);
 }
